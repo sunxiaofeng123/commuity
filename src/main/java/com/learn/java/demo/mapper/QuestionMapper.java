eb127389917a9fde9c83cceddc3199b045d72695
sunxiaofeng123 <18703137630@163.com>
2019-09-13 02:39:08 +0800

    问题提交

---
+++ src/main/java/com/learn/java/demo/mapper/QuestionMapper.java
@@ -0,0 +1,11 @@
+package com.learn.java.demo.mapper;
+
+import com.learn.java.demo.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert(" insert into (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmt_create},#{gmt_modified},#{creator},#{tag})")
+    void create(Question question);
+}
