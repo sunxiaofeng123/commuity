afb24cff3695fd6e2466685be9481d9512ef8698
sunxiaofeng123 <18703137630@163.com>
2019-09-13 19:02:51 +0800

    新增头像

--- src/main/java/com/learn/java/demo/controller/AuthorizeController.java
+++ src/main/java/com/learn/java/demo/controller/AuthorizeController.java
@@ -54,6 +54,7 @@ public class AuthorizeController {
             String token = UUID.randomUUID().toString();
             user.setToken(token);
             user.setName(githubUser.getName());
+            user.setAvatarUrl(githubUser.getAvatar_url());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
